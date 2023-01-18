d41593e70873ef12219884d8cbf276e8789ef3ed
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-01-18 01:00:32 +0000

    populated scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,24 @@
 var highscores = document.getElementById("highscores");
+var clear = document.getElementById("clear");
+var theBest = localStorage
 
-var theBest = localStorage.
+// TO DO: SORT object
+
+var listToPrint =""
+// Iterate through the object
+for (const key in theBest) {
+    if (theBest.hasOwnProperty(key)) {
+      listToPrint += `<li>`;
+      listToPrint += `${key}: ${theBest[key]}`;
+      listToPrint += `</li>`;
+    }
+}
+
+highscores.innerHTML = listToPrint;
+
+
+
+
+clear.addEventListener("click", function(event) {
+    localStorage.clear();
+})
